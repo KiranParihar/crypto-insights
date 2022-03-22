59d67579a9748e15d6cb61ccd3fe60aebcd6fa91
KiranParihar <62878055+KiranParihar@users.noreply.github.com>
2022-03-23 00:19:29 +0530

    First commit

---
+++ src/config/FirebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDMAFimSGGnfT4yCqqZFJ3T7ESTcoiA_Tc",
+    authDomain: "crypto-insights-a526e.firebaseapp.com",
+    projectId: "crypto-insights-a526e",
+    storageBucket: "crypto-insights-a526e.appspot.com",
+    messagingSenderId: "414414481214",
+    appId: "1:414414481214:web:fc89ad4b110bec5645ee6f",
+    measurementId: "G-8F5P9EYT5X"
+  };
+
+  export default firebaseConfig;
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth }from "firebase/auth"
+
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import { getFirestore } from 'firebase/firestore';
+import firebaseConfig from "./config/FirebaseConfig"
+
+const firebaseApp = initializeApp(firebaseConfig)
+
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
+
+export { auth, db };
